235d92182eb25aae4a3f3f23fe7d9396b76f451a
BylaneA <billieabdiw@gmail.com>
2018-04-26 16:49:50 +0200

    checking

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Quizz</title>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+
+      <header>
+        <div class="container">
+          <h1>Quizz PHP</h1>
+        </div>
+      </header>
+
+      <main>
+        <div class="container">
+          <h2>Testons un peu ce que vous valez en PHP</h2>
+          <p>Quiiz a multiple choix</p>
+          <ul>
+            <li>Nombre de question: 5</li>
+            <li>Type: Choix multiple</li>
+            <li>Temps: 5 minutes</li>
+          </ul>
+          <a href="question.php" class="start">Commencer</a>
+        </div>
+      </main>
+
+      <footer>
+        <div class="container">
+          Done by Bilane
+        </div>
+      </footer>
+
+    </body>
+
+
+</html>
