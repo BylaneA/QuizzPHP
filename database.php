79f02a7e762ce76264cc2c71e80659e4e3db4d87
BylaneA <billieabdiw@gmail.com>
2018-06-05 16:25:21 +0200

    Getting the question from the database

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = 'quizz_db';
+$db_user = 'root';
+$db_pass = 'facesimplon';
+
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if($mysqli->connect_error){
+
+	printf("Connect failed: %s\n", $mysqli->connect_error);
+	exit();
+}
+?>
